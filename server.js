d2b962296bff5ae4cb6318f0b3c00346629d3eb4
Yuvraj Singh <yuvrajsinghwahla08cs@gmail.com>
2023-06-05 23:38:04 +0530

    port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 
 var cors = require('cors');
 var path = require('path');
-var port = 3504;
+var port = 3505;
 
 const Razorpay = require('razorpay');
 const { response } = require('express');
